bc9baba1cb68ba1479b84908f6cada49a4feb5d1
Haikal Khairuddin <z4nyx@outlook.com>
2020-10-23 12:36:19 +0000

    camera: fix new version
    Also make it optional
    
    Co-authored-by: Qiwu Huang <huangqiwu@xiaomi.com>
    Signed-off-by: Haikal Khairuddin <z4nyx@outlook.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -306,6 +306,12 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 	struct msm_camera_slave_info *slave_info;
 	const char *sensor_name;
 	uint16_t temp_sid = 0;
+	uint16_t vcmid = 0;
+	int have_vcmid = 0;
+#ifdef CONFIG_XIAOMI_NEWCAM
+        uint16_t lensid = 0;
+	int have_lensid = 0;
+#endif
 	enum cci_i2c_master_t temp_master = MASTER_0;
 	if (!s_ctrl) {
 		pr_err("%s:%d failed: %pK\n",
@@ -473,6 +479,19 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 			&vendorid,
 			s_ctrl->sensordata->vendor_id_info->data_type);
 	}
+
+#ifdef CONFIG_XIAOMI_NEWCAM
+    if (s_ctrl->sensordata->lens_id_info->lens_id_addr != 0)
+	{
+	    msm_camera_cci_i2c_read(
+		sensor_i2c_client,
+		s_ctrl->sensordata->lens_id_info->lens_id_addr,
+		&lensid,
+		s_ctrl->sensordata->lens_id_info->data_type);
+		have_lensid = 1;
+	}
+#endif
+
 	sensor_i2c_client->cci_client->sid = temp_sid;
 	sensor_i2c_client->cci_client->cci_i2c_master = temp_master;
 	if (rc < 0) {
@@ -481,7 +500,44 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 	}
 	if (s_ctrl->sensordata->vendor_id_info->vendor_id != vendorid) {
 		rc = -1;
-		pr_err("%s: read vendor id: 0x%x expected id 0x%x:\n",
+		return rc;
+	}
+	else
+	{
+		if(have_vcmid == 1)
+		{
+			if (s_ctrl->sensordata->vcm_id_info->vcm_id != vcmid)
+			{
+				pr_err("%s:match vcmid if failed read vcm id: 0x%x expected id 0x%x:\n",
+				__func__, vcmid, s_ctrl->sensordata->vcm_id_info->vcm_id);
+				rc = -1;
+				return rc;
+			}
+			else
+			{
+				pr_err("%s: read vcmid id: 0x%x expected id 0x%x:\n",
+				__func__, vcmid, s_ctrl->sensordata->vcm_id_info->vcm_id);
+			}
+		}
+#ifdef CONFIG_XIAOMI_NEWCAM
+        if(have_lensid == 1)
+		{
+			if (s_ctrl->sensordata->lens_id_info->lens_id != lensid)
+			{
+				pr_err("%s:match lensid if failed read lens id: 0x%x expected id 0x%x:\n",
+				__func__, lensid, s_ctrl->sensordata->lens_id_info->lens_id);
+				rc = -1;
+				return rc;
+			}
+			else
+			{
+				pr_err("%s: read lensid id: 0x%x expected id 0x%x:\n",
+				__func__, lensid, s_ctrl->sensordata->lens_id_info->lens_id);
+			}
+		}
+#endif
+	}
+	pr_err("%s: read vendor id: 0x%x expected id 0x%x:\n",
 			__func__, vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id);
 	}
 	return rc;
